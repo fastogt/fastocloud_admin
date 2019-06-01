3805db5aff120662877a46a9276294a73ecafc29
topilski <atopilski@gmail.com>
2019-06-01 01:52:55 -0400

    1.0.0 Release

--- app/config/public_config.py
+++ app/config/public_config.py
@@ -2,4 +2,4 @@ PUBLIC_CONFIG = {'site': {'title': 'FastoCloud', 'keywords': 'video,cloud,iptv,s
                  'company': {'title': 'FastoGT', 'domain': 'https://fastogt.com'},
                  'support': {'contact_email': 'support@fastogt.com',
                              'contact_address': 'Republic of Belarus, Minsk, Stadionnay str. 5'},
-                 'project': {'version': '1.4.2', 'version_type': 'rc'}}
+                 'project': {'version': '1.0.0', 'version_type': 'release'}}
