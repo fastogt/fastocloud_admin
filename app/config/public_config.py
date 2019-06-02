a500c7f7e8433fbfe53744887fcfbe0e1976aeba
topilski <atopilski@gmail.com>
2019-06-02 19:06:35 +0300

    Community channel

--- app/config/public_config.py
+++ app/config/public_config.py
@@ -1,5 +1,6 @@
 PUBLIC_CONFIG = {'site': {'title': 'FastoCloud', 'keywords': 'video,cloud,iptv,service,restreamer'},
                  'company': {'title': 'FastoGT', 'domain': 'https://fastogt.com'},
                  'support': {'contact_email': 'support@fastogt.com',
-                             'contact_address': 'Republic of Belarus, Minsk, Stadionnay str. 5'},
+                             'contact_address': 'Republic of Belarus, Minsk, Stadionnay str. 5',
+                             'community_channel': 'https://discord.gg/cnUXsws'},
                  'project': {'version': '1.1.0', 'version_type': 'rc'}}
